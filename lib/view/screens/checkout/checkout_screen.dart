df549ffee036c9cc658c960857b9bce90b91add4
dron-001 <44139501+dron-001@users.noreply.github.com>
2022-08-26 16:37:13 +0300

    Вырезал добавление промо кода

--- lib/view/screens/checkout/checkout_screen.dart
+++ lib/view/screens/checkout/checkout_screen.dart
@@ -350,7 +350,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
 
 
                         // Coupon
-                        GetBuilder<CouponController>(builder: (couponController) {
+                      /*  GetBuilder<CouponController>(builder: (couponController) {
                             return Container(
                               color: Theme.of(context).cardColor,
                               padding: EdgeInsets.symmetric(horizontal: Dimensions.PADDING_SIZE_SMALL),
@@ -450,7 +450,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                               ]),
                             );
                           },
-                        ),
+                        ),*/
                         SizedBox(height: Dimensions.PADDING_SIZE_EXTRA_SMALL),
 
                         (orderController.orderType != 'take_away' && Get.find<SplashController>().configModel.dmTipsStatus == 1) ?
