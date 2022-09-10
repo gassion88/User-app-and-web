2e945c3ef91773ff4086c021341623919e900616
dron-001 <44139501+dron-001@users.noreply.github.com>
2022-09-10 15:50:21 +0300

    Вырезал выбор языка при первом запуске приложения

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -120,6 +120,5 @@ class AppConstants {
 
   static List<LanguageModel> languages = [
     LanguageModel(imageUrl: Images.english, languageName: 'English', countryCode: 'US', languageCode: 'en'),
-    LanguageModel(imageUrl: Images.arabic, languageName: 'عربى', countryCode: 'SA', languageCode: 'ar'),
   ];
 }
