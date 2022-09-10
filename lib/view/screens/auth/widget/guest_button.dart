dee023854a8456decb58fc38ae4b71db95bef0a9
dron-001 <44139501+dron-001@users.noreply.github.com>
2022-09-10 15:56:28 +0300

    Сделан вход без показа экрана с картой

--- lib/view/screens/auth/widget/guest_button.dart
+++ lib/view/screens/auth/widget/guest_button.dart
@@ -1,7 +1,10 @@
+import 'package:efood_multivendor/controller/location_controller.dart';
+import 'package:efood_multivendor/data/model/response/address_model.dart';
 import 'package:efood_multivendor/helper/route_helper.dart';
 import 'package:efood_multivendor/util/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GuestButton extends StatelessWidget {
   @override
@@ -10,8 +13,13 @@ class GuestButton extends StatelessWidget {
       style: TextButton.styleFrom(
         minimumSize: Size(1, 40),
       ),
-      onPressed: () {
-        Navigator.pushReplacementNamed(context, RouteHelper.getInitialRoute());
+      onPressed: () async {
+       AddressModel _address = await Get.find<LocationController>()
+            .getCurrentLocation(true,
+                defaultLatLng: LatLng(42.2128383, 43.9553117));
+        Get.find<LocationController>()
+            .saveAddressAndNavigate(_address, false, '/', '/' != null);
+        // Navigator.pushReplacementNamed(context, RouteHelper.getInitialRoute());
       },
       child: RichText(text: TextSpan(children: [
         TextSpan(text: '${'continue_as'.tr} ', style: robotoRegular.copyWith(color: Theme.of(context).disabledColor)),
